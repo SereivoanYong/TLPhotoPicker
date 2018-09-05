19a5f46b1d5c194ecd5486b6d573824e64c267be
Sereivoan Yong <sereivoanyong@gmail.com>
2018-09-05 09:50:55 +0700

    Re-indent

--- TLPhotoPicker/Classes/TLBundle.swift
+++ TLPhotoPicker/Classes/TLBundle.swift
@@ -9,12 +9,12 @@
 import Foundation
 
 class TLBundle {
-    class func podBundleImage(named: String) -> UIImage? {
-        let podBundle = Bundle(for: TLBundle.self)
-        if let url = podBundle.url(forResource: "TLPhotoPickerController", withExtension: "bundle") {
-            let bundle = Bundle(url: url)
-            return UIImage(named: named, in: bundle, compatibleWith: nil)!
-        }
-        return nil
+  class func podBundleImage(named: String) -> UIImage? {
+    let podBundle = Bundle(for: TLBundle.self)
+    if let url = podBundle.url(forResource: "TLPhotoPickerController", withExtension: "bundle") {
+      let bundle = Bundle(url: url)
+      return UIImage(named: named, in: bundle, compatibleWith: nil)!
     }
+    return nil
+  }
 }
--- TLPhotoPicker/Classes/TLCollectionTableViewCell.swift
+++ TLPhotoPicker/Classes/TLCollectionTableViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class TLCollectionTableViewCell: UITableViewCell {
-    @IBOutlet var thumbImageView: UIImageView!
-    @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var subTitleLabel: UILabel!
+  @IBOutlet var thumbImageView: UIImageView!
+  @IBOutlet var titleLabel: UILabel!
+  @IBOutlet var subTitleLabel: UILabel!
 }
